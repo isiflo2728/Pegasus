fed00304d836a3c1546f3befc83e3fd67db511bd
isiflo2728 <iflore110@calstatela.edu>
2026-03-10 18:29:56 -0700

    Initial Commit

---
+++ Pegasus/PegasusApp.swift
@@ -0,0 +1,17 @@
+//
+//  PegasusApp.swift
+//  Pegasus
+//
+//  Created by Isidoro Flores on 3/10/26.
+//
+
+import SwiftUI
+
+@main
+struct PegasusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PegasusTests/PegasusTests.swift
@@ -0,0 +1,17 @@
+//
+//  PegasusTests.swift
+//  PegasusTests
+//
+//  Created by Isidoro Flores on 3/10/26.
+//
+
+import Testing
+@testable import Pegasus
+
+struct PegasusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
