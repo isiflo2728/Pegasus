9ebdaa29f39b8e1614d0c8fb5bb3d8a12744cc2b
isiflo2728 <iflore110@calstatela.edu>
2026-03-12 13:54:49 -0700

    CustomTabBar

---
+++ Pegasus/Helpers/CustomTabBar.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTabBar.swift
+//  Pegasus
+//
+//  Created by Isidoro Flores on 3/12/26.
+//
+
+import SwiftUI
+
+struct CustomTabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTabBar()
+}
---
+++ Pegasus/Views/FeedView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedView.swift
+//  Pegasus
+//
+//  Created by Isidoro Flores on 3/11/26.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedView()
+}
